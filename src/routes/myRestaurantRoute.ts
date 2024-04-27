42d6c21abf7a5e2075e8ef7fe1abd94fa991995e
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-04-27 19:45:09 +0200

    Updates

--- src/routes/myRestaurantRoute.ts
+++ src/routes/myRestaurantRoute.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import multer from 'multer';
 import MyRestaurantController from '../Controllers/MyRestaurantController';
 import { jwtCheck, jwtParse } from '../middleware/auth';
